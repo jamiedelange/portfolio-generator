7f9032bef37d9156771bd61822e0632fcdac3047
jelly octane <jamie.de.lange@mac.com>
2020-11-28 19:21:54 -0500

    project section

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template.js');
 
-//const pageHTML = generatePage(name, github);
-
-//fs.writeFile('./index.html', pageHTML, err => {
-    //if (err) throw err;
-
-    //console.log('Portfolio complete! Check out index.html to see the output!');
-//}); 
-
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -143,9 +135,9 @@ promptUser()
     .then(portfolioData => {
         const pageHTML = generatePage(portfolioData);
 
-        //fs.writeFile('./index.html', pageHTML, err => {
-            //if (err) throw new Error(err);
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
 
-            //console.log('Page created! Check out index.html in this directory to see it!');
-        //});
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
